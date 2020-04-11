a668af83db00799a34d47759c3e9d3199090c680
gcgbarbosa <gcgbarbosa@gmail.com>
2020-04-11 13:22:56 -0700

    first added the pdf extraction part

--- code/main.py
+++ code/main.py
@@ -4,6 +4,10 @@ import pdfminer
 
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
 
 # read and write strings as files
 output_string = StringIO()
@@ -14,5 +18,12 @@ with open('data/ba/01/20200107.pdf', 'rb') as in_file:
     # get the pdf doc 
     doc = PDFDocument(parser)
     # 
-    rsrcmr = PDFResourceManager()
+    rsrcmgr = PDFResourceManager()
     # TODO: follow this https://pdfminersix.readthedocs.io/en/latest/tutorials/composable.html
+    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+    #
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+
+    print(output_string.getvalue())

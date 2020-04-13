c543a7181841e353878410bae15cfe14e6bc90fa
gcgbarbosa <gcgbarbosa@gmail.com>
2020-04-12 21:43:20 -0700

    added code in spacy to split sentences

--- code/main.py
+++ code/main.py
@@ -12,7 +12,37 @@ from pdfminer.pdfdevice import PDFDevice
 
 
 
-def fromPDFToText(path):
+class Document:
+    def __init__(self, doc_id):
+        self.pages = 0
+        # using an empty dict to store the sentences
+        self.sentences = dict()
+        self.doc_id = doc_id
+    
+    def addPage(self, sentences, page=None):
+        # TODO: implement this function
+        return 0
+    
+
+    def getAllSenteces(self):
+        return self.sentences
+
+
+def textToSentences(text):
+    import spacy
+    # loading the portugese model
+    nlp = spacy.load("pt_core_news_sm")
+    # parsej
+    doc = nlp(text)
+    # return sentences
+    return [str(sent) for sent in list(doc.sents)]
+    #return list(doc.sents)
+
+sentences = textToSentences("Esse e um exemplo de texto. Esse e outro exemplo de texto.")
+assert(sentences == ['Esse e um exemplo de texto.', 'Esse e outro exemplo de texto.'])
+
+
+def fromPDFToDocument(path):
     with open(path, 'rb') as in_file:
         # parse file
         parser = PDFParser(in_file)
@@ -29,6 +59,8 @@ def fromPDFToText(path):
         device = PDFPageAggregator(rsrcmgr, laparams=laparams)
         # create a pdf interpreter object
         interpreter = PDFPageInterpreter(rsrcmgr, device)
+        # declare the document
+        document_obj = Document('fakeid')
         # process each page contained in the document
         for page in PDFPage.create_pages(document):
             # process the page
@@ -37,12 +69,18 @@ def fromPDFToText(path):
             # get the text of an entire page.
             layout = device.get_result()
             #
+            page_text = ''
             for element in layout:
                 # we don't want some elements
                 if not type(element).__name__ in ['LTFigure', 'LTRect', 'LTLine', 'LTCurve']:
-                    # print the elements of interest
-                    print( element.get_text())
+                    # save the text of the page
+                    page_text = page_text + ' ' + element.get_text().replace('\n', '')
+                    
+            # convert the text of the page to a sentence list 
+            sentences = textToSentences(page_text)
+            # TODO: document_obj.addPage(sentences) 
+            print(sentences)
 
 
-
-fromPDFToText('data/ba/01/20200107.pdf')
+fromPDFToDocument('data/ba/01/20200107.pdf')
+# TODO: add the assertion

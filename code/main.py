fbe45991a8ff76f657987614554043fc53c57134
gcgbarbosa <gcgbarbosa@gmail.com>
2020-04-17 03:46:05 -0700

    added a little bit more to the pdf conversion function

--- code/main.py
+++ code/main.py
@@ -16,20 +16,34 @@ class Document:
     def __init__(self, doc_id):
         self.pages = 0
         # using an empty dict to store the sentences
-        self.sentences = dict()
         self.doc_id = doc_id
+        self.pages = []
     
-    def addPage(self, sentences, page=None):
+    def addPage(self, sentences):
         # TODO: implement this function
+        self.pages.append(sentences)
         return 0
 
-    # TODO function to return an interator to deal with the senteces
+    ## TODO: instead of a list this should return an iterator in the future
+    def getPage(self, page_number):
+        # -1 here because the data structure is zero indexed and we want to mantain coherence
+        return self.pages[page_number - 1]
+
+    def getNumPages(self):
+        return len(self.pages)
 
     # TODO function to return an iterator to deal with the pages
+    def getAllSentences(self):
+        sentences = []
+        for page in self.pages:
+            for sentence in page:
+                sentences.append(sentence)
 
-    def getAllSenteces(self):
-        return self.sentences
+        return sentences
 
+    def getNumSentences(self):
+        # TODO: do a proper job here
+        return len(self.getAllSentences())
 
 def textToSentences(text):
     import spacy
@@ -64,7 +78,10 @@ def fromPDFToDocument(path):
         # declare the document
         document_obj = Document('fakeid')
         # process each page contained in the document
+        n_pages = 1
         for page in PDFPage.create_pages(document):
+            print('processing page ', n_pages)
+            n_pages = n_pages + 1
             # process the page
             interpreter.process_page(page)
             # get the entire text of the page?
@@ -80,9 +97,46 @@ def fromPDFToDocument(path):
                     
             # convert the text of the page to a sentence list 
             sentences = textToSentences(page_text)
-            # TODO: document_obj.addPage(sentences) 
-            print(sentences)
+            # add sentences to the page
+            document_obj.addPage(sentences)
+        
+        # returning the document obj
+        return document_obj
+
+tt = fromPDFToDocument('data/ba/01/20200107.pdf')
+
+assert(tt.getNumPages() == 64)
+assert(tt.getNumSentences() == 9709)
+
+class Match:
+    def __init__(self, document_id, page, document_link, state, matched_text, date):
+        self.document_id = document_id
+        self.page = page
+        self.document_link = document_link
+        self.state = state
+        self.matched_text = matched_text
+        self.date = date
+
+# TODO: complete the search function
+## Search function
+def search_term(document_list, query):
+    return 0
+    # for each document in the list
+
+    # for each sentence in each document
+
+    # if query matches the sentence
+
+    # create a match object
+    ## the object should have:
+    ## document id
+    ## sentence matched + sentences around for manual review
+    ## page matched
+    ## day matched
+    ## link to the document
+    ## state matched
+
+    ## such information should come from the file structure itself
 
 
-fromPDFToDocument('data/ba/01/20200107.pdf')
-# TODO: add the assertion
+

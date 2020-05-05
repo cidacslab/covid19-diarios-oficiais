d8023d8835bdd190cbb118fc3f2030fbdbca4fc5
gcgbarbosa <gcgbarbosa@gmail.com>
2020-05-04 21:03:46 -0700

    added search class

---
+++ code/slp/search.py
@@ -0,0 +1,11 @@
+from document import Document
+
+class Search:
+    def __init__(self, index='default'):
+        # set index
+        this.index = index
+    
+    ''' here doc is of time Document
+    '''
+    def index_document(self, doc):
+        

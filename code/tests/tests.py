234293aa5efdd61dbcae12f9d95ee3afe86ca02e
gcgbarbosa <gcgbarbosa@gmail.com>
2020-05-04 21:13:18 -0700

    trying to fix tests

--- code/tests/tests.py
+++ code/tests/tests.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('slp')
 
-from main import textToSentences
+from pdf_processor import PDFProcessor 
 
 # see https://github.com/CleanCut/green#unit-test-structure-tutorial
 import unittest
@@ -23,8 +23,10 @@ class TestExamples(unittest.TestCase):
 '''
 class TestNLP(unittest.TestCase):
     def test_textToSencenes(self):
+        # now we need an object
+        pdfproc = PDFProcessor()
         ## testing text to sentence func
-        sentences = textToSentences("Esse e um exemplo de texto. Esse e outro exemplo de texto.")
+        sentences = pdfproc.text_to_sentences("Esse e um exemplo de texto. Esse e outro exemplo de texto.")
         self.assertEqual(sentences, ['Esse e um exemplo de texto.', 'Esse e outro exemplo de texto.'])
 
 
@@ -32,8 +34,9 @@ class TestNLP(unittest.TestCase):
 '''
 class TestIE(unittest.TestCase):
     def test_fromPDFToDocument(self):
+        pdfproc = PDFProcessor()
         ## testing fromPDFToDocument func
-        tt = fromPDFToDocument('data/test.pdf')
+        tt = pdfproc.pdf_to_document('data/test.pdf')
         # this document has 64 pagesj
         self.assertEqual(tt.getNumPages(), 5)
         # this document has 9709 sentences

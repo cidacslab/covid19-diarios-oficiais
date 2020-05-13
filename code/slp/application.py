c789a67f842b31b1fbaba83cb55f326cde1d71c9
gcgbarbosa <gcgbarbosa@gmail.com>
2020-05-13 13:23:59 -0700

    template is fine now

--- code/slp/application.py
+++ code/slp/application.py
@@ -5,6 +5,8 @@ from io import StringIO
 #
 from flask import Flask, render_template
 
+import datetime
+
 app = Flask(__name__)
 
 ## todo FLASK
@@ -12,13 +14,26 @@ app = Flask(__name__)
 @app.route('/<query>')
 def q(query=None):
     hits = None
+    base = datetime.datetime.today()
+    num_days = 100
+    hits_per_date = {(base - datetime.timedelta(days=x)).strftime('%Y-%m-%d'):0 for x in range(num_days)}
+     
     if query:
         search = Search()
         # search.index_document(doc)
         
         hits = search.search_term(query)
+        # generate graph
+        # group by date
+        # print num hits
+        all_dates = [h.date[:4] + '-' + h.date[4:6] + '-' + h.date[6:] for h in hits]
+        for date in all_dates:
+            if date not in hits_per_date:
+                hits_per_date[date] = 0
+            hits_per_date[date] = hits_per_date[date] + 1
 
-    return render_template('index.html', hits=hits, query=query)
+
+    return render_template('index.html', hits=hits, query=query, graph=hits_per_date)
 
 
 
